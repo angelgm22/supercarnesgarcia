e43c672308980dba4bb8ca69ec0ccfe66aa8466c
angelgarzam <angelgm22@gmail.com>
2025-12-13 16:44:46 -0600

    1.0.2

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/supercarnesgarcia/',
+  base: '/',
   build: {
     outDir: 'dist',
     emptyOutDir: true,
